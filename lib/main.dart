06a4b71e9fe518665b4aaa38f9301424bbbea6a2
steelwsky <zero.rise@yandex.ru>
2019-12-20 19:52:07 +1000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:mynewspod/news_feed.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyNewsFeed(title: 'Reddit top feed'),
+    );
+  }
+}
+
