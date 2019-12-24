4f40a74259a5e8bc80cad182eda7623dd66b4551
steelwsky <zero.rise@yandex.ru>
2019-12-24 19:26:53 +1000

    found suitable rss. Need to add STAR icon on the right side of textViews. TODO provider, tabs (news/favs).

--- lib/controller.dart
+++ lib/controller.dart
@@ -2,17 +2,19 @@ import 'package:http/http.dart' as http;
 import 'package:webfeed/webfeed.dart';
 import 'package:flutter/foundation.dart';
 
-final url = 'http://www.bbc.co.uk/music/genres/rockandindie/reviews.rss';
+//final url = 'http://www.bbc.co.uk/music/genres/rockandindie/reviews.rss';
+//final url = 'https://itsallwidgets.com/podcast/feed';
+final url = 'http://www.cnbc.com/id/19789731/device/rss/rss.xml';
 
 class News with ChangeNotifier {
-  AtomFeed _feed;
-  AtomItem _selectedItem;
+  RssFeed _feed;
+  RssItem _selectedItem;
 
-  AtomFeed get feed => _feed;
+  RssFeed get feed => _feed;
 
-  AtomItem get selectedItem => _selectedItem;
+  RssItem get selectedItem => _selectedItem;
 
-  set selectedItem(AtomItem value) {
+  set selectedItem(RssItem value) {
     _selectedItem = value;
     notifyListeners();
   }
@@ -20,7 +22,7 @@ class News with ChangeNotifier {
   void parse() async {
     final res = await http.get(url);
     final xmlStr = res.body;
-    _feed = AtomFeed.parse(xmlStr);
+    _feed = RssFeed.parse(xmlStr);
     notifyListeners();
   }
 
